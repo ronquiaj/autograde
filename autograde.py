e587a31d563db907c4314c82d5cf83fe9bde3f58
Adrian Ronquillo <ronquiaj@plu.edu>
2021-08-25 13:30:35 -0500

    Separated autograde.py file into functions, and created sakai.py

--- autograde.py
+++ autograde.py
@@ -13,45 +13,75 @@ assignment_num = input("Enter an assignment number: ")
 
 browser = webdriver.Chrome("C:\\bin\\chromedriver.exe")
 
-browser.get("https://learn.zybooks.com/signin")
+"""
+Function which goes to and logs in to Zybooks with provided credentials
+"""
+def login_to_zybooks(browser):
+    browser.get("https://learn.zybooks.com/signin")
 
-email = browser.find_element_by_id("ember10")
-password = browser.find_element_by_id("ember12")
-submit_auth = browser.find_element_by_class_name("title")
+    email = browser.find_element_by_id("ember10")
+    password = browser.find_element_by_id("ember12")
+    submit_auth = browser.find_element_by_class_name("title")
+    
+    email.send_keys(EMAIL)
+    password.send_keys(PASSWORD)
+    submit_auth.click()
 
-email.send_keys(EMAIL)
-password.send_keys(PASSWORD)
-submit_auth.click()
+"""
+Goes to the assignments tab, clicks on the report button
+"""
+def navigate_to_dropdown_window(browser):
+    browser.get("https://learn.zybooks.com/zybook/PLUCSCI144CaoSpring2021")
+    time.sleep(2)
+    assignments_tab = browser.find_elements_by_class_name("full-tab")[-1]
+    assignments_tab.click()
+    time.sleep(2)
+    report_button = browser.find_element_by_xpath('//*[@id="ember176"]/div[1]/div[2]/button')
+    report_button.click()
 
+"""
+Configures the dropdowns to match the desired class, and specifies that we want the second section of this class
+"""
+def configure_dropdowns(browser):
+    dropdowns = browser.find_elements_by_class_name("ember-basic-dropdown-trigger")
+    browser.implicitly_wait(2)
+    topics = dropdowns[0]
+    students = dropdowns[1]
+    
+    topics.click()
+    first_drop_down_options = browser.find_elements_by_class_name("ember-power-select-option")
+    click_in_dropdown(first_drop_down_options, assignment_num)
+
+    students.click()
+    section2 = browser.find_elements_by_class_name("ember-power-select-option")[2]
+    section2.click()
+
+"""
+Finds the download button and clicks download
+"""
+def download_csv(browser):
+    download_assignment_report_button = browser.find_elements_by_class_name("zb-button")
+    for i in download_assignment_report_button:
+        if (i.text == 'Download assignment report'):
+            i.click()
+            break
+
+login_to_zybooks(browser)
 time.sleep(2)
-browser.get("https://learn.zybooks.com/zybook/PLUCSCI144CaoSpring2021")
+navigate_to_dropdown_window(browser)
 time.sleep(2)
+configure_dropdowns(browser)
+time.sleep(2)
+download_csv(browser)
 
-assignments_tab = browser.find_elements_by_class_name("full-tab")[-1]
 
-assignments_tab.click()
 
-report_button = browser.find_element_by_xpath('//*[@id="ember176"]/div[1]/div[2]/button')
-report_button.click()
 
-dropdowns = browser.find_elements_by_class_name("ember-basic-dropdown-trigger")
-browser.implicitly_wait(2)
-topics = dropdowns[0]
-students = dropdowns[1]
 
-topics.click()
-first_drop_down_options = browser.find_elements_by_class_name("ember-power-select-option")
-click_in_dropdown(first_drop_down_options, assignment_num)
 
-students.click()
-section2 = browser.find_elements_by_class_name("ember-power-select-option")[2]
-section2.click()
 
-download_assignment_report_button = browser.find_elements_by_class_name("zb-button")
-for i in download_assignment_report_button:
-    if (i.text == 'Download assignment report'):
-        i.click()
-        break
+
+
 
 time.sleep(100000)
 
---
+++ sakai.py
@@ -0,0 +1,56 @@
+import time
+from selenium import webdriver
+import os
+from dotenv import load_dotenv
+import re
+
+load_dotenv()
+SAKAI_USERNAME = os.getenv("SAKAI_USERNAME")
+SAKAI_PASSWORD = os.getenv("SAKAI_PASSWORD")
+
+desired_class = input("Enter the name of the class you'd like to click on: ")
+browser = webdriver.Chrome("C:\\bin\\chromedriver.exe")
+
+
+"""
+Logs in to Sakai through the guest route
+"""
+def login(browser):
+   
+    browser.get("https://sakai.plu.edu/portal")
+
+    guest_login_button = browser.find_element_by_id("loginLink2")
+    guest_login_button.click()
+    
+    time.sleep(1)
+
+    username = browser.find_element_by_id("eid")
+    password = browser.find_element_by_id("pw")
+    submit_login_button = browser.find_element_by_id("submit")
+
+    username.send_keys(SAKAI_USERNAME)
+    password.send_keys(SAKAI_PASSWORD)
+    submit_login_button.click()
+
+"""
+Iterates through the Sakai tabs on the home page and uses a regex comparison against our query to click on the desired tab
+"""
+def iterate_through_tabs_and_click(browser, desired_class):
+    
+    class_regex = re.compile(desired_class, re.IGNORECASE)
+    
+    tabs = browser.find_elements_by_class_name("Mrphs-sitesNav__menuitem") # The tabs visible in the main menu
+    for tab in tabs:
+        if (class_regex.search(tab.text)):
+            tab.click()
+            break
+        
+login(browser)
+iterate_through_tabs_and_click(browser, desired_class)
+
+time.sleep(10000)
+
+
+
+
+
