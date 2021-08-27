079869611ec72fcc871ffa730551cd80edade7e4
Adrian Ronquillo <ronquiaj@plu.edu>
2021-08-27 18:47:22 -0500

    Added regex to test against our desired assignment num and assignment we are looking for in the assignments folder

--- read_csv.py
+++ read_csv.py
@@ -1,21 +1,38 @@
 import csv
 import os
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
 
 PATH = os.getenv("ASSIGNMENTS_PATH")
+assignment_regex = re.compile('(?<=Week_)[0-9]+', re.IGNORECASE)
 
-assignments = os.listdir(PATH)
-assignment_path = PATH + '\\' + assignments[0]
+"""
+Function which uses regex to find the corresponding assignment against our assignment number, returns the path to that assignment
+"""
+def get_assignment_path(assignment_num):
+    assignments = os.listdir(PATH)
+    for assignment in assignments:
+        num = assignment_regex.search(assignment).group(0) # Use the regex to get the assignment number for this file
+        if str(num) == str(assignment_num):
+            return assignment
+    raise ValueError("Assignment was not found, please try again")
+            
+    
 
-students_and_grades = {}
+"""
+Given an assignment number, looks through our assignments folder and finds the corresponding csv. Builds an object containing students mapped to their grades for that week
+"""
+def get_grades(assignment_num):
+    assignment_path = PATH + "\\" + get_assignment_path(assignment_num)
+    students_and_grades = {}
 
-with open(assignment_path, 'r') as csvfile:
-    datareader = csv.reader(csvfile)
-    for row in datareader:
-        if row[0] == "Last name": continue # Skip the first headers
-        full_name = row[0] + ", " + row[1]
-        students_and_grades[full_name] = row[6]
+    with open(assignment_path, 'r') as csvfile:
+        datareader = csv.reader(csvfile)
+        for row in datareader:
+            if row[0] == "Last name": continue # Skip the first headers
+            full_name = row[0] + ", " + row[1]
+            students_and_grades[full_name] = row[6]
 
-print(students_and_grades)
+    return students_and_grades
