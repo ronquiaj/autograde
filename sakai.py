be74a2d88f6f8c05ada6e936110efd8ac865d7b3
Adrian Ronquillo <ronquiaj@plu.edu>
2021-08-29 14:26:51 -0500

    Implemented functionality to paste grade into the sakai gradebook

--- sakai.py
+++ sakai.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 import os
 from dotenv import load_dotenv
 import re
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
 load_dotenv()
@@ -12,7 +13,9 @@ SAKAI_USERNAME = os.getenv("SAKAI_USERNAME")
 SAKAI_PASSWORD = os.getenv("SAKAI_PASSWORD")
 
 desired_class = input("Enter the name of the class you'd like to click on: ")
-browser = webdriver.Chrome("C:\\bin\\chromedriver.exe")
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument("--start-maximized")
+browser = webdriver.Chrome("C:\\bin\\chromedriver.exe", options=chromeOptions)
 
 """
 Logs in to Sakai through the guest route.
@@ -24,7 +27,7 @@ def login(browser):
     guest_login_button = browser.find_element_by_id("loginLink2")
     guest_login_button.click()
     
-    time.sleep(1)
+    time.sleep(3)
 
     username = browser.find_element_by_id("eid")
     password = browser.find_element_by_id("pw")
@@ -67,21 +70,39 @@ def iterate_through_students(browser, assignment_num):
     
     browser.execute_script("document.querySelector('.wtHolder').scrollBy(2000, 1000)")
 
-    time.sleep(3)
+    time.sleep(2)
     column_headers = browser.find_elements_by_class_name("gb-title")
-    header_index = 0
+    time.sleep(2)
+    assignment_index = 0
+    cur_index = 0
     for column_header in column_headers:
         assignment = column_header.get_attribute("title")
-        if column_header.is_displayed():
-           if (str(assignment_num) in assignment):
-               print(assignment)
-    
-    # Get rows
+        if ("Assign" in assignment):
+            if (str(assignment_num) in assignment): # Find the column of the assignment we are looking for
+                assignment_index = cur_index
+                break
+            else:
+                cur_index += 1
+       
+
     rows = browser.find_elements_by_css_selector("tr")
+    time.sleep(2)
+    c = 0
     for row in rows:
-        columns = row.find_elements_by_css_selector("td")
-        for column in columns:
-            print(column.text)
+        columns = row.find_elements_by_css_selector("div.relative > span.gb-value")
+        try:
+            if (len(columns) > 0):
+                grade_cell = columns[assignment_index + 1]
+                print("Text:", grade_cell.text, ", Displayed:", grade_cell.is_displayed(), ", Enabled:", grade_cell.is_enabled())
+                # ActionChains(browser).move_to_element(grade_cell).perform()
+                ActionChains(browser).move_to_element(grade_cell).click(grade_cell).send_keys("100").send_keys(Keys.ENTER).perform()
+                if (c == 5):
+                  break
+                c += 1
+           
+        except IndexError:
+            break
+    print("Done")
         
 
 login(browser)
