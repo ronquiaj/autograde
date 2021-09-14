b437e7f058d67abf1f20037cdfe2777ef97af326
Adrian Ronquillo <ronquiaj@plu.edu>
2021-09-14 12:46:58 -0500

    More sh changes

--- autograde.sh
+++ autograde.sh
@@ -3,5 +3,5 @@
 read -p 'Assignment for CSCI 144 Section 2: ' assignment1
 read -p 'Assignment for CSCI 144 Section 3: ' assignment2
 
-python autograde.py "CS144 Introduction to Computer Science Fall2021" $assignment1 "https://learn.zybooks.com/zybook/PLUCSCI144Fall2021" 2 & 
-python autograde.py "CS 144 - 03 Fall 2021" $assignment2 "https://learn.zybooks.com/zybook/PLUCSCI144Fall2021" 3 &
+python autograde.py "CS144 Introduction to Computer Science Fall2021" $assignment1 "https://learn.zybooks.com/zybook/PLUCSCI144Fall2021" 2 &> output.log & 
+python autograde.py "CS 144 - 03 Fall 2021" $assignment2 "https://learn.zybooks.com/zybook/PLUCSCI144Fall2021" 3 &> output.log &
